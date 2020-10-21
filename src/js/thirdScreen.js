007f2b97025e597730f41738ab1e3c1597bf76f2
Георгий <george.kozlov@zennolab.com>
2020-10-21 10:13:08 +0300

    show submenu from left

--- src/js/thirdScreen.js
+++ src/js/thirdScreen.js
@@ -1,10 +1,14 @@
 const servicesSection = document.getElementById('services');
 const titles = servicesSection.querySelectorAll('.services__title');
+const servicesBlocks = servicesSection.querySelectorAll('.services__block');
+
+const servicesScreenDefaultHeight1920 = 520;
 
 export default function initThirdScreenInteractivity() {
   if (window.screen.width >= 1920) {
     calculateServicesScreenHeight();
     bindServicesTitlesToClick();
+    bindSubmenuToClick();
   }    
 }
 
@@ -15,7 +19,7 @@ function calculateServicesScreenHeight() {
   const allArticlesHeightProps = [...allArticles].map((article) => article.offsetHeight);
   const maxArticleHeight = Math.max(...allArticlesHeightProps);
 
-  servicesScreen.style.height = `${maxArticleHeight}px`;
+  servicesScreen.style.height = `${maxArticleHeight >= servicesScreenDefaultHeight1920 || servicesScreenDefaultHeight1920}px`;
 
   if (servicesWrap.offsetHeight >= window.screen.height) {
     servicesSection.style.height = 'unset';
@@ -30,7 +34,39 @@ function bindServicesTitlesToClick() {
 
 function handleTitleClick(e) {
   const targetTitle = e.currentTarget;
+  const targetTitleIndex = [...titles].indexOf(targetTitle);
+  const targetBlock = servicesBlocks[targetTitleIndex];
 
+  markTitle(targetTitle);
+  hideAllServicesBlocks();
+  showTargetServicesBlock(targetBlock);
+}
+
+function markTitle(targetTitle) {
   titles.forEach((title) => title.classList.remove('services__title_active'));
   targetTitle.classList.add('services__title_active');
+}
+
+function hideAllServicesBlocks() {
+  servicesBlocks.forEach((block) => {
+    block.classList.remove('services__block_active')
+  })
+}
+
+function showTargetServicesBlock(targetBlock) {
+  targetBlock.classList.add('services__block_active')
+}
+
+function bindSubmenuToClick() {
+  const submenu = servicesSection.querySelectorAll('.services__item-header');
+  submenu.forEach((item) => item.addEventListener('click', handleSubmenuClick)) 
+}
+
+function handleSubmenuClick(e) {
+  const target = e.currentTarget.parentElement;
+  const parentWrap = target.offsetParent;
+  const otherSubmenuInParentWrap = parentWrap.querySelectorAll('.services__block-item');
+
+  otherSubmenuInParentWrap.forEach((submenu) => submenu.classList.remove('services__block-item_active'));
+  target.classList.add('services__block-item_active');
 }
