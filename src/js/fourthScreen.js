05213388c50df346e19373275514f6e410d62e5d
Георгий <george.kozlov@zennolab.com>
2020-10-24 00:05:52 +0300

    demo

--- src/js/fourthScreen.js
+++ src/js/fourthScreen.js
@@ -4,8 +4,10 @@ const projectArticlesList = projectsSection.querySelector('#projectsArticleList'
 const projectsCoverSwitcherUp = document.getElementById('projectsCoverSwitcherUp');
 const projectsCoverSwitcherDown = document.getElementById('projectsCoverSwitcherDown');
 const projectsCovers = projectsSection.querySelectorAll('.projects__cover-item');
+const coverBlock = projectsSection.querySelector('.projects__cover-block');
+const projectsArticles = projectArticlesList.querySelectorAll('.projects__article-wrap');
 
-let mainCoverIndex = 0;
+let mainCoverIndex = 2;
 
 export default () => {
     calculateArticleHeight();
@@ -21,8 +23,8 @@ function calculateArticleHeight() {
 
 function bindCoversSwitchers() {
     if (window.screen.width >= 1160) {
-        projectsCoverSwitcherUp.addEventListener('click', incrementMainCoverIndex);
-        projectsCoverSwitcherDown.addEventListener('click', decrementMainCoverIndex);
+        projectsCoverSwitcherUp.addEventListener('click', decrementMainCoverIndex);
+        projectsCoverSwitcherDown.addEventListener('click', incrementMainCoverIndex);
     }
 }
 
@@ -30,12 +32,35 @@ function incrementMainCoverIndex() {
     if (++mainCoverIndex > 3) {
         mainCoverIndex = 0;
     }
+
     switchCover();
+    switchArticle(0);
 }
 
 function decrementMainCoverIndex() {
     if (--mainCoverIndex < 0) {
         mainCoverIndex = 3;
     }
+
     switchCover();
+    switchArticle(1);
+}
+
+function switchCover() {
+    const prevMainCover = coverBlock.querySelector('.projects__cover-item_not-nested');
+    const futureMainCover = projectsCovers[mainCoverIndex];
+
+    const prevMainClasses = prevMainCover.classList.value.split(' ');
+    const futureMainClasses = futureMainCover.classList.value.split(' ');
+
+    prevMainCover.classList.remove(...prevMainClasses);
+    prevMainCover.classList.add(...futureMainClasses);
+
+    futureMainCover.classList.remove(...futureMainClasses);
+    futureMainCover.classList.add(...prevMainClasses);
+}
+
+function switchArticle(fromUp) {
+    const targetArticle = projectsArticles[mainCoverIndex];
+    console.log(targetArticle);
 }
